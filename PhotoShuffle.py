bc573660af10e865e50985afb0ebb80e5e15ef46
Daniel <endlessly.curious@gmail.com>
2011-05-09 13:51:46 -0700

    Cleaned up with PyLint.

--- PhotoShuffle.py
+++ PhotoShuffle.py
@@ -1,70 +1,69 @@
-"""Scans a given folder and its children and builds a sorted fodler structure based on meta data in images."""
+"""Scans a folder and builds a sorted structure based on image creation time."""
 
-from os import walk,name as osname
-from os.path import splitext,getctime,join,isfile
+from os import walk, name as osname
+from os.path import splitext, getctime, join, isfile
 from sys import argv
 from datetime import datetime
 from PIL import Image
 from PIL.ExifTags import TAGS
-from subprocess import Popen,PIPE
+from subprocess import Popen, PIPE
 
 def get_creation_time(path):
     """Get the creation time of a file, uses stat on unix."""
     timestamp = 0
     if osname != 'posix':
-        timestamp = getctime( filename )
+        timestamp = getctime( path )
     else:
-        p = Popen(['stat', '-f%B', path],
+        process = Popen(['stat', '-f%B', path],
             stdout=PIPE, stderr=PIPE)
-        if p.wait():
-            raise OSError(p.stderr.read().rstrip())
+        if process.wait():
+            raise OSError(process.stderr.read().rstrip())
         else:
-            timestamp = int(p.stdout.read())
+            timestamp = int(process.stdout.read())
     return datetime.fromtimestamp( timestamp ) 
 
-def get_exif(fn):
+def get_exif(path):
     """Get embedded EXIF data from image file."""
     ret = {}
     try:
-        i = Image.open(fn)
-        if hasattr( i, '_getexif' ):
-            info = i._getexif()
-            if info != None:
-                for tag, value in info.items():
+        img = Image.open(path)
+        if hasattr( img, '_getexif' ):
+            exifinfo = img._getexif()
+            if exifinfo != None:
+                for tag, value in exifinfo.items():
                     decoded = TAGS.get(tag, tag)
                     ret[decoded] = value
         else:
-            print 'NO EXIF ' + fn
+            print 'NO EXIF ' + path
     except IOError:
-        print 'IOERROR ' + fn
+        print 'IOERROR ' + path
     return ret
 
 if __name__ == '__main__':
     OLD = argv[1]
 
     print 'Scanning ' + OLD 
-    exts = {}
-    noinfo = 0
-    withinfo = 0
+    NOINFO = 0
+    WITHINFO = 0
     for root, dirs, files in walk( OLD ):
         if 'iPhoto Library' not in root:
             for name in files:
-                filename = join(root,name)
+                filename = join(root, name)
                 if isfile( filename ):
-                    # record number of different file types (by extension).
                     ext = splitext( name )[1].lower()    
-                    exts[ext] = exts.get( ext, 0 ) + 1 
-                    if ext in ['.jpg','.jpeg','.png','.tiff']:
+                    if ext in ['.jpg', '.jpeg', '.png', '.tiff']:
                         info = get_exif( filename )
-                        if ('DateTime' in info.keys()) or ('DateTimeOriginal' in info.keys()):
+                        # Get creation time from EXIF data or from OS.
+                        if 'DateTimeOrginal' in info.keys():
+                            WITHINFO += 1
+                        elif 'DateTime' in info.keys():
                             #print filename
                             #print info['DateTime']
-                            withinfo += 1
+                            WITHINFO += 1
                         else:
                             print filename
                             print get_creation_time( filename )
-                            noinfo += 1
-    print exts 
-    print withinfo
-    print noinfo
+                            NOINFO += 1
+    print WITHINFO
+    print NOINFO
 
